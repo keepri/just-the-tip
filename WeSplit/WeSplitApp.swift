e5a9e33a4061d5ba21a8b6df79560f95143e3df2
KIPRI <hi@kipri.dev>
2023-06-05 11:53:07 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ciprian-Liviu Pîțul on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
